68c3c130d35cff5ca23ab18ba1fd5fca807edfa7
Simon Martinelli <simon@72.services>
2018-12-21 13:39:35 +0100

    'init'

---
+++ src/main/java/com/example/demogradlejpa/DemoGradleJpaApplication.java
@@ -0,0 +1,14 @@
+package com.example.demogradlejpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoGradleJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoGradleJpaApplication.class, args);
+    }
+
+}
+
---
+++ src/main/java/com/example/demogradlejpa/Employee.java
@@ -0,0 +1,17 @@
+package com.example.demogradlejpa;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Data
+@Entity
+public class Employee {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private String name;
+}
